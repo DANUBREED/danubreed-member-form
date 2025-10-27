70b2d48bf11e4fb560e6a9d440e651a713074a44
Ak David <akdavid4real@gmail.com>
2025-10-27 15:03:25 +0100

    fixed

--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_BASE_URL = "https://daniels-camp-backend.onrender.com/api/v1"
+const API_BASE_URL = "https://daniels-camp-backend.onrender.com"
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
